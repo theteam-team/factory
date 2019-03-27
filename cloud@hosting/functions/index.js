aa0da2d79442ae3487aecf3864433a83b58f19d0
mohamedgamal500 <46355891+mohamedgamal500@users.noreply.github.com>
2019-03-27 19:06:56 +0200

    hosting

---
+++ cloud@hosting/functions/index.js
@@ -0,0 +1,29 @@
+const functions = require('firebase-functions');
+const firebase = require('firebase-admin');
+const express = require('express');
+
+const firebaseApp = firebase.initializeApp(
+    functions.config().firebase
+);
+
+
+    const ref = firebaseApp.database().ref('/new');
+    const data = ref.once('value').then(snap => snap.val());
+
+
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/time', (request, response)=>{
+    response.send('odres in console of cloud functions firebase website');
+    
+    console.log(data);
+    
+    console.log('it is ....working......');
+
+});
+
+
+exports.app = functions.https.onRequest(app);
