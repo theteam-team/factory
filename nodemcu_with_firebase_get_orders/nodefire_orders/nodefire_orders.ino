88c65fc710c6d4a338f63ec986b82af2a9dfeb35
Muhammad114Adel <48260661+Muhammad114Adel@users.noreply.github.com>
2019-04-21 17:11:41 +0200

    Update nodefire_orders.ino

--- nodemcu_with_firebase_get_orders/nodefire_orders/nodefire_orders.ino
+++ nodemcu_with_firebase_get_orders/nodefire_orders/nodefire_orders.ino
@@ -1,5 +1,8 @@
 #include <FirebaseArduino.h>
 #include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
+#include <Servo.h>
+
 
 #define WIFI_SSID "your_network_name"
 #define WIFI_PASSWORD "your_network_password"
@@ -9,6 +12,68 @@
 #define FIREBASE_AUTH "your_database_secret_key"
 
 
+Servo myservo_red;  // create servo object to control a servo
+Servo myservo_green;  // create servo object to control a servo
+Servo myservo_yellow;  // create servo object to control a servo
+
+    
+// counting the red balls 
+void count_red(int num){
+ int angle = 70;
+  for(int i=0; i<num; i++){
+    Serial.println("the current ball is red "  );
+    for (int pos = 0; pos <= angle; pos += 1) { 
+      delay(20);
+      myservo_red.write(pos);             
+                            
+    }
+    for (int pos = angle; pos <= 0; pos -= 1) {  
+      myservo_red.write(pos);             
+      delay(20);                      
+    }
+    delay(1000);
+  }
+}
+//---------------------------------------------------------------------------------------------------------------------------------
+// counting the green balls 
+void count_green(int num){
+ 
+int angle = 70;
+  for(int i=0; i<num; i++){
+    Serial.println("the current ball is green "  );
+    for (int pos = 0; pos <= angle; pos += 1) { 
+      delay(20);
+      myservo_green.write(pos);             
+                            
+    }
+    for (int pos = angle; pos <= 0; pos -= 1) {  
+      myservo_green.write(pos);             
+      delay(20);                      
+    }
+    delay(1000);
+  }
+}
+
+
+//---------------------------------------------------------------------------------------------------------------------------------
+// counting the yellow balls 
+void count_yellow(int num){
+  
+ int angle = 70;
+  for(int i=0; i<num; i++){
+    Serial.println("the current ball is yellow "  );
+    for (int pos = 0; pos <= angle; pos += 1) { 
+      delay(20);
+      myservo_yellow.write(pos);             
+                            
+    }
+    for (int pos = angle; pos <= 0; pos -= 1) {  
+      myservo_yellow.write(pos);             
+      delay(20);                      
+    }
+    delay(1000);
+  }
+}
 
 void setup() {
   
@@ -28,19 +93,23 @@ void setup() {
   
 }
 
+
 void loop() {
   //get data red_balls
   int red = Firebase.getInt("node/red");
+  count_red(red);
   Serial.print("RED :  ");
   Serial.print(red);
   
   //get data green_balls
   int green = Firebase.getInt("node/green");
+   count_green(green);
   Serial.print("  GREEN :  ");
   Serial.print(green);
 
   //get data yellow_balls
   int yellow = Firebase.getInt("node/yellow");
+  count_yellow(yellow);
   Serial.print("  YELLOW :  ");
   Serial.println(yellow);
  
